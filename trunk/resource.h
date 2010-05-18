8d593fd5cabf3bb1593cd5faf85cbe3b2eab6b90
happyday19c <happyday19c@f92c9028-33b8-4ca2-afdf-7b78eec7ef35>
2010-05-18 09:22:24 +0000

    메뉴 아이템 추가 및 예제 도형 데이터를 추가함

--- trunk/resource.h
+++ trunk/resource.h
@@ -5,17 +5,35 @@
 #define IDD_ABOUTBOX                    100
 #define IDP_OLE_INIT_FAILED             100
 #define IDS_STRING101                   101
+#define IDS_RIBBON_PANEL_DIRECTION1     101
+#define IDS_RIBBON_DIR_UP               102
+#define IDS_RIBBON_DIR_DOWN             103
+#define IDS_RIBBON_DIR_LEFT             104
+#define IDS_RIBBON_DIR_RIGHT            105
+#define IDS_RIBBON_PANEL_DIRECTION2     106
+#define IDS_RIBBON_DIR_RDOWN            107
+#define IDS_RIBBON_DIR_RUP              108
+#define IDS_RIBBON_DIR_LUP              109
 #define IDB_WRITESMALL                  110
+#define IDS_RIBBON_DIR_LDOWN            110
 #define IDB_WRITELARGE                  111
+#define ID_DIR_UP                       111
 #define IDB_MAIN                        112
+#define ID_DIR_DOWN                     112
 #define IDB_BUTTONS                     113
+#define ID_DIR_LEFT                     113
 #define IDB_FILELARGE                   114
+#define ID_DIR_RIGHT                    114
 #define IDB_FILESMALL                   115
+#define ID_DIR_RUP                      115
+#define ID_DIR_RDOWN                    116
+#define ID_DIR_LUP                      117
+#define ID_DIR_LDOWN                    118
 #define IDR_POPUP_EDIT                  119
+#define IDS_RIBBON_PANEL_DIRECTION3     119
 #define ID_STATUSBAR_PANE1              120
 #define ID_STATUSBAR_PANE2              121
 #define IDS_STATUS_PANE1                122
-#define IDS_STATUS_PANE2                123
 #define IDR_MAINFRAME                   128
 #define IDR_MAINFRAME_256               129
 #define IDR_My2DTransTYPE               130
@@ -66,16 +84,25 @@
 #define IDS_RIBBON_STATUSBAR            289
 #define IDS_RIBBON_VIEW                 290
 #define IDS_EDIT_MENU                   306
+#define IDB_TRANLARGE                   314
 #define ID_WRITE_PASTEASHYPERLINK       32770
 #define ID_32771                        32771
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
 #define IDS_RIBBON_CATEGORY_TRANSLATE   57346
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        314
-#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_RESOURCE_VALUE        315
+#define _APS_NEXT_COMMAND_VALUE         32791
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
