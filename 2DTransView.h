2c571d329eeb7c861d344f0fbcdf3ed661a4bbf2
Happyday19c <Happyday19c@f92c9028-33b8-4ca2-afdf-7b78eec7ef35>
2010-05-19 20:35:55 +0000

    버전 관리를 위한 업로드

--- 2DTransView.h
+++ 2DTransView.h
@@ -45,12 +45,12 @@ protected:
 // 생성된 메시지 맵 함수
 protected:
 	afx_msg void OnFilePrintPreview();
-	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
-	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
 	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
-	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint point);
 	DECLARE_MESSAGE_MAP()
 public:
+	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint point);
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
 	afx_msg void OnDirUp();
